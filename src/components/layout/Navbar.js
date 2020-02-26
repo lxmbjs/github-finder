4d1c6cacd87d9488ab997e630343f819640bf039
Ryan Lambacher <ryan.lambacher@gmail.com>
2020-02-26 15:26:34 -0500

    Added a lot

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { NavLink } from 'react-router-dom';
 const Navbar = ({ icon, title }) => {
   return (
     <nav className='navbar bg-primary'>
@@ -8,6 +8,14 @@ const Navbar = ({ icon, title }) => {
         <i className={icon} />
         {title}
       </h1>
+      <ul>
+        <li>
+          <NavLink to='/'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='/about'>About</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
